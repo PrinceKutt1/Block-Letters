f5b180e0a883a5bf83ff8d642d394c12d554d7f1
Kutorglo Prince Wells <87288156+PrinceKutt1@users.noreply.github.com>
2021-12-24 04:52:13 +1100

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,12 @@
+#@Twitter : Prince_kutt1
+# He's reserved and very controversial
+
+
+
+print("  P P P  P          K          K ")
+print("  P         P       K        K   ")
+print("  P         P       K     K  ")
+print("  P  P  P  P        K   K    ")
+print("  p                 K     K     ")
+print("  P                 K        K   ")
+print("  P                 K          K ")
